71ae017cd3b92132761a2ff38b4958961f0ad232
Darren Judd <darren@raonline.co.uk>
2013-10-16 15:25:59 +0100

    Changed forward slashes

--- phpExample/test.php
+++ phpExample/test.php
@@ -14,24 +14,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="shortcut icon" href="/images/favicon.png">
+    <link rel="shortcut icon" href="images/favicon.png">
 
     <title>Grid Template for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="/css/bootstrap.css" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="/css/grid.css" rel="stylesheet">
+    <link href="css/grid.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
-      <script src="/js/html5shiv.js"></script>
-      <script src="/js/respond.min.js"></script>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
     <![endif]-->
   </head>
 
   <body>
+
     <div class="container">
 
       <div class="page-header">
@@ -124,7 +125,7 @@
     <!-- Bootstrap core JavaScript
 ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="/js/jquery-1.10.2.js" type="text/javascript"></script>
-    <script src="/js/bootstrap.js" type="text/javascript"></script>
+    <script src="js/jquery-1.10.2.js" type="text/javascript"></script>
+    <script src="js/bootstrap.js" type="text/javascript"></script>
   </body>
 </html>
