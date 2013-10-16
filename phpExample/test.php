89e8facc48e3d68d5465656486c7bb046d790de4
Adrian Graham <adrian@raonline.co.uk>
2013-10-16 15:20:48 +0100

    Added jQuery

--- phpExample/test.php
+++ phpExample/test.php
@@ -124,7 +124,7 @@
     <!-- Bootstrap core JavaScript
 ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="js/jquery-1.10.2.js" type="text/javascript"></script>
-    <script src="js/bootstrap.js" type="text/javascript"></script>
+    <script src="/js/jquery-1.10.2.js" type="text/javascript"></script>
+    <script src="/js/bootstrap.js" type="text/javascript"></script>
   </body>
 </html>
